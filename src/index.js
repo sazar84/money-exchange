87fded68c1ecbbd5bb5b3533a052b94ec6474954
sazar84 <45146694+sazar84@users.noreply.github.com>
2019-03-03 16:24:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,23 @@ module.exports = function makeExchange(currency) {
     if (currency > 10000) {
         return {error: "You are rich, my friend! We don't have so much coins for exchange"};
     }
+
+    if (currency <= 0) {
+        return {};
+    }
     
+    const coins = new Map(
+        [['H', 50],
+        ['Q', 25],
+        ['D', 10],
+        ['N', 5],
+        ['P', 1]]
+    );
+
+    let change = {};
+
+
+
+    return change;
 }
     
