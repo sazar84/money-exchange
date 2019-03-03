231b89443ded840b2979c608a42e83beb7162662
sazar84 <45146694+sazar84@users.noreply.github.com>
2019-03-03 21:12:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,25 +3,31 @@ module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
     if (currency > 10000) {
-        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+        return {
+            error: "You are rich, my friend! We don't have so much coins for exchange"
+        };
     }
 
     if (currency <= 0) {
         return {};
     }
-    
-    const coins = new Map(
-        [['H', 50],
+
+    let coins = new Map([
+        ['H', 50],
         ['Q', 25],
         ['D', 10],
         ['N', 5],
-        ['P', 1]]
-    );
+        ['P', 1]
+    ]);
 
     let change = {};
 
+    coins.forEach((coinValue, coinName) => {
+        if (currency == coinValue) {
+            change[coinValue];
+        }
 
+    });
 
     return change;
 }
-    
