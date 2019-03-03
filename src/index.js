52323d399c969c59b209bc821b54c9a5c4d0a522
sazar84 <45146694+sazar84@users.noreply.github.com>
2019-03-03 16:02:08 +0300

    add

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    
 }
+    
